f6ea5aae52c97848d68883dd673638794efb4bf9
enriqueMota <abelmota@live.com>
2022-09-17 16:11:37 -0400

    added custom dropdown

--- src/components/posts/AllPosts.tsx
+++ src/components/posts/AllPosts.tsx
@@ -1,25 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { RefObject, useEffect, useRef, useState } from "react";
 import useGetPosts from "../../api/useGetPosts";
 
 interface AllPostsProps {}
 
 const AllPosts: React.FunctionComponent<AllPostsProps> = () => {
+  const dropdownContentRef = useRef<any>(null);
+  const dropdownRef = useRef<any>(null);
   const [params, setParams] = useState({
     page: 0,
     query: "reactjs",
   });
   const { data } = useGetPosts(params);
+  const openDropdown = () => {
+    dropdownContentRef!.current!.style!.display = "block";
+  };
+  const closeDropdown = () => {
+    dropdownContentRef!.current!.style!.display = "none";
+  };
+  const handleClick = () => {
+    closeDropdown();
+  };
+  
 
   return (
-    <div>
-      holasdf
-      {data?.hits.map((hit) => (
+    <div id="parent">
+      <div
+        onBlur={closeDropdown}
+        onClick={openDropdown}
+        ref={dropdownRef}
+        className="dropdown"
+      >
+        <button className="dropbtn">
+          <span>Select your news</span>
+          <img
+            height={10}
+            src="/img/arrow-down-sign-to-navigate.png"
+            alt="down-arrow"
+          />
+        </button>
+        <div ref={dropdownContentRef} className="dropdown-content">
+          <div onClick={handleClick} className="dropdown-option">
+            <img src="/img/image-138.png" alt="angular-logo" />
+            <span>Angular</span>
+          </div>
+          <div onClick={handleClick} className="dropdown-option">
+            <img src="/img/image-140.png" alt="reactjs-logo" />
+            <span>Reactjs</span>
+          </div>
+          <div onClick={handleClick} className="dropdown-option">
+            <img src="/img/image-141.png" alt="vuejs-logo" />
+            <span>Vuejs</span>
+          </div>
+        </div>
+      </div>
+      {/* {data?.hits.map((hit) => (
         <>
           <span>{hit?._highlightResult?.comment_text?.value}</span>
           <br />
           <br />
         </>
-      ))}
+      ))} */}
     </div>
   );
 };
