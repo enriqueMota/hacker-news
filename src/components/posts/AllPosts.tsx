ada22445f758678e7ce86a4c4fa237f09bb23583
enriqueMota <abelmota@live.com>
2022-09-17 22:30:41 -0400

    infinite scrolling implementation

--- src/components/posts/AllPosts.tsx
+++ src/components/posts/AllPosts.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Waypoint } from "react-waypoint";
 import useGetPosts from "../../api/useGetPosts";
+import { Hit } from "../../models";
 import Dropdown from "../layout/Dropdown";
 import Post from "./Post";
 
@@ -7,22 +9,34 @@ interface AllPostsProps {}
 
 const AllPosts: React.FunctionComponent<AllPostsProps> = () => {
   const [params, setParams] = useState({ page: 0, query: "reactjs" });
+  const [posts, setPosts] = useState<Hit[]>();
   const { data } = useGetPosts(params);
+  const notNullable = ['author', 'story_title', 'story_url', 'created_at']
 
   useEffect(() => {
-    window.onscroll = function (ev) {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        console.log('bottom')
-      }
-    };
-  }, []);
+    if(posts?.length) {
+      
+      setPosts([...posts, ...data?.hits as Hit[]])
+      return;
+    }
+    setPosts(data?.hits)
+  }, [data?.hits]);
 
   return (
     <div>
       <Dropdown {...{ setParams }} />
       <div className="Post-Container">
-        {data?.hits?.map((hit, i) => (
-          <Post key={`${hit?.story_id}-${i}`} {...{ hit }} />
+        {posts?.map((hit, i) => (
+          <React.Fragment key={`${hit?.story_id}-${i}`}>
+            <Post {...{ hit }} />
+            {posts?.length === i + 1 && (
+              <Waypoint
+                onEnter={() => {
+                  setParams({ ...params, page: params?.page + 1 });
+                }}
+              />
+            )}
+          </React.Fragment>
         ))}
       </div>
     </div>
